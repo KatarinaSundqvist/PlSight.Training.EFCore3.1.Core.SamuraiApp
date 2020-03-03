2261bac5dab48ac2492540d4f87821ea7d8249c7
KatarinaSundqvist <singsongkat@live.com>
2020-03-03 10:23:41 +0100

    Setting things up to use our DBContext in the API

---
+++ SamuraiApp.Data/SamuraiContext.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using SamuraiApp.Domain;
+
+namespace SamuraiApp.Data {
+    public class SamuraiContext : DbContext {
+        public DbSet<Samurai> Samurais { get; set; }
+        public DbSet<Quote> Quotes { get; set; }
+        public DbSet<Clan> Clans { get; set; }
+        public DbSet<Battle> Battles { get; set; }
+        public DbSet<SamuraiBattleStat> SamuraiBattleStats { get; set; }
+
+        public static readonly ILoggerFactory ConsoleLoggerFactory = LoggerFactory.Create(builder => {
+            builder
+                .AddFilter((category, level) =>
+                    category == DbLoggerCategory.Database.Command.Name
+                    && level == LogLevel.Information)
+                .AddConsole();
+        });
+
+
+        // Hard-coded connection string. Just for demo and first looks!
+        // For real software, use dependency injection
+        // The EnableSensitiveDataLogging is also only for internal use! Be careful!
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+            optionsBuilder
+                .UseLoggerFactory(ConsoleLoggerFactory)
+                .EnableSensitiveDataLogging()
+                .UseSqlServer("Server = (localdb)\\mssqllocaldb; Database = SamuraiAppDataCore");
+        }
+
+        // Here we are using Fluent API to specify the last critical detail of
+        // the many-to-many relationship between Battles and Samurais
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+            modelBuilder.Entity<SamuraiBattle>().HasKey(s => new { s.SamuraiId, s.BattleId });
+            modelBuilder.Entity<Horse>().ToTable("Horses");
+            // EF Core will find the Horse class without it having a DbSet property
+            // because it's linked to the Samurai class
+            // but it would name the table Horse, so we change that here
+            // (our business rules don't want anyone to be able to interact directly with the horse class, so we don't want to expose it via DbSet)
+            modelBuilder.Entity<SamuraiBattleStat>().HasNoKey().ToView("SamuraiBattleStats");
+            // the first part shows that it is intentional that this entity doesn't have a key
+            // The second part is to tell the application that there is already a view for this entity. Otherwise, the next migration we apply will try to create a table
+            // HasNoKey entities will never be tracked by EF Core, and if you try to force it with AsTracking, EF Core will ignore your attempt
+        }
+    }
+}
